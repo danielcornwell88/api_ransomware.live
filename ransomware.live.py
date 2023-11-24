98d369cfb79b3818aff5a9ed8e64065328b9acb9
danielcornwell88 <56154691+danielcornwell88@users.noreply.github.com>
2023-11-24 20:21:02 +1000

    Create ransomware.live.py

---
+++ ransomware.live.py
@@ -0,0 +1,33 @@
+import requests
+
+class ransomware_live_api_wrapper:
+	def __init__(self):
+		self.base = "https://api.ransomware.live"
+
+	def country(self,country_id):
+		response = requests.get(self.base+'/country/{0}'.format(country_id))
+		return response.json()
+
+	def group(self,group_name):
+		response = requests.get(self.base+'/group/{0}'.format(group_name))
+		return response.json() 
+
+	def groups(self):
+		response = requests.get(self.base+'/groups')
+		return response.json()
+
+	def groupvictims(self,group_names):
+		response = requests.get(self.base+'/groupvictims/{0}'.format(group_names))
+		return response.json()
+
+	def recentcyberattacks(self):
+		response = requests.get(self.base+'/recentcyberattacks')
+		return response.json()
+
+	def recentvictims(self):
+		response = requests.get(self.base+'/recentvictims')
+		return response.json()
+
+	def victims(self,year, month):
+		response = requests.get(self.base+'/victims/{0}/{1}'.format(year,month))
+		return response.json()
